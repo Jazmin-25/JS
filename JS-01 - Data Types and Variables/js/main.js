be3adfe7643b9d8a0ff1ffc0ffb6b073c6499b7b
Jaz <jaz.islasg@gmail.com>
2023-04-05 23:49:47 -0600

    Se resuelve el primer desafio

---
+++ JS-01 - Data Types and Variables/js/main.js
@@ -0,0 +1,10 @@
+//Desafio 1: crear una calculadora de edad
+console.log("Desafio 1: crea una calculadora de edad");
+
+let nacimiento = 1995
+let añoFuturo = 2046
+resta = añoFuturo - nacimiento;
+console.log("Tendrás:", resta + " años" + " en el " + añoFuturo);
+
+//Desafio 2: crear una tarjeta de información
+console.log("Desafio 2: crear una tarjeta de información");
