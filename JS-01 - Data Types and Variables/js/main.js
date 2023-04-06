7d691949912e359237ffe09eff5e9fcb5d9a0e8f
Jaz <jaz.islasg@gmail.com>
2023-04-06 00:39:56 -0600

    Se resuelve parte del segundo desafio. Falta ligarlo a la card

--- JS-01 - Data Types and Variables/js/main.js
+++ JS-01 - Data Types and Variables/js/main.js
@@ -6,5 +6,19 @@ let añoFuturo = 2046
 resta = añoFuturo - nacimiento;
 console.log("Tendrás:", resta + " años" + " en el " + añoFuturo);
 
-//Desafio 2: crear una tarjeta de información
+//Desafio 2: crear una tarjeta de información con 3 piezas de información: nombre, ubicacion y pasatiempo.
 console.log("Desafio 2: crear una tarjeta de información");
+let nombre = "John"
+let ubicacion = " vivo en Sudáfrica"
+let pasatiempo = "disfruto bebiendo vino casero"
+console.log("Hola, mi nombre es " + nombre + ubicacion + " y " + pasatiempo)
+document.write ("Hola, mi nombre es " + nombre + ubicacion + " y " + pasatiempo)
+//let card = document.getElementById("card-template");
+
+{
+    nombre; "Ariadna",
+    ubicacion; "Vivo en México",
+    pasatiempo; "disfruto ver series y peliculas"
+
+}
+
